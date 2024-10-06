955463b207f8c2f2ce57268301f3d4075896ef71
KartoffelChips <jan@strassburger.org>
2024-10-06 11:07:39 +0200

    added Modrinthstats export

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import Modrinth from "./Modrinth";
 import { Project, License, DonationUrl, GalleryItem } from "./interfaces/project";
+import {ModrinthStatistics} from "./interfaces/miscellaneous"
 
 export {
     Modrinth,
     Project,
     License,
     DonationUrl,
-    GalleryItem
+    GalleryItem,
+    ModrinthStatistics,
 }
---
+++ src/interfaces/miscellaneous/index.ts
@@ -0,0 +1,5 @@
+import ModrinthStatistics from "./ModrinthStatistics";
+
+export {
+    ModrinthStatistics
+}
