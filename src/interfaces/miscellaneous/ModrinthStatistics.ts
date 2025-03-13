18b5e2b29071b0cc12c15717096d1690ef030b4a
KartoffelChips <jan@strassburger.org>
2025-03-13 21:20:14 +0100

    Updated ModrinthStatistics interface

--- src/interfaces/miscellaneous/ModrinthStatistics.ts
+++ src/interfaces/miscellaneous/ModrinthStatistics.ts
@@ -1,6 +1,16 @@
+/**
+ * Represents the statistics of Modrinth
+ */
 export default interface ModrinthStatistics {
+    /** Number of projects on Modrinth */
     projects: number;
+
+    /** Number of versions on Modrinth */
     versions: number;
+
+    /** Number of version files on Modrinth */
     files: number;
+
+    /** Number of authors (users with projects) on Modrinth */
     authors: number;
 }
